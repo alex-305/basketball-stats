940a7a876bbad5db44ed462d459e0ab4829dde47
Alex Gonzalez <galex2003@icloud.com>
2024-08-19 10:39:45 -0400

    Setup tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import NavBar from './components/NavBar'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [ count, setCount ] = useState(1)
 
   return (
     <>
-      <NavBar/>
-      <button onClick={() => setCount(count+1)}>{count}</button>
+      <div>
+        <NavBar/>
+      </div>
     </>
   )
 }
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,7 +3,13 @@
 function NavBar() {
     return (
         <>
-        <div>Hoop Swish</div>
+            <div className="mx-auto w-fit">
+                <button>
+                    <span 
+                    className="text-3xl text-bold text-mono text-center"
+                    >Hoop Swish</span>
+                </button>
+            </div>
         </>
     )
 }
